542e9a6a914b4803b7f1e341f5b7e9af2ab7ee9b
tbohne <tim.bohne@dfki.de>
2025-05-06 10:08:12 +0200

    impl config

---
+++ saliency_kd/config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author Tim Bohne
+
+ONTOLOGY_PREFIX = "http://www.semanticweb.org/sensor_fault_ontology#"
+FUSEKI_URL = "http://127.0.0.1:3030"
+SPARQL_ENDPOINT = "/saliency_kd/sparql"
+DATA_ENDPOINT = "/saliency_kd/data"
+UPDATE_ENDPOINT = "/saliency_kd/update"
