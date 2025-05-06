64504d40e2d0b471d0b942d40b78d1521066721f
tbohne <tim.bohne@dfki.de>
2025-05-06 10:34:58 +0200

    extend KG generator -- symbolic properties for all classes

--- saliency_kd/knowledge_graph_generator.py
+++ saliency_kd/knowledge_graph_generator.py
@@ -16,6 +16,62 @@ SYMBOLIC_FAULT_INFO = {
         "name": "class_1",
         "fault_desc": "no obvious positive / negative peaks; on average, the signal roughly ends how it started",
         "severity": "X"
+    },
+    2: {
+        "name": "class_2",
+        "fault_desc": "a positive peak towards the start followed by a huge negative drop (below the starting values);"
+                      "the difference between the min and max value is around 500",
+        "severity": "X"
+    },
+    3: {
+        "name": "class_3",
+        "fault_desc": "similar to class_2, but not as extreme",
+        "severity": "X"
+    },
+    4: {
+        "name": "class_4",
+        "fault_desc": "similar to class_2 and class_3, but even weaker",
+        "severity": "X"
+    },
+    5: {
+        "name": "class_5",
+        "fault_desc": "first a small drop, then a positive peak, stabilizing on the high values",
+        "severity": "X"
+    },
+    6: {
+        "name": "class_6",
+        "fault_desc": "more or less straight, then a heavy drop, stabilizing on low values",
+        "severity": "X"
+    },
+    7: {
+        "name": "class_7",
+        "fault_desc": "straight, drop, increase, starting and ending the same - ' bath tub'",
+        "severity": "X"
+    },
+    8: {
+        "name": "class_8",
+        "fault_desc": "slightly up, drop below start, then straight",
+        "severity": "X"
+    },
+    9: {
+        "name": "class_9",
+        "fault_desc": "straight, up, drop, slightly up (below start values)",
+        "severity": "X"
+    },
+    10: {
+        "name": "class_10",
+        "fault_desc": "straight, tiny drop, positive peak, drop - stabilized",
+        "severity": "X"
+    },
+    11: {
+        "name": "class_11",
+        "fault_desc": "straight, up, about twice as much down, and up again - stabilized",
+        "severity": "X"
+    },
+    12: {
+        "name": "class_12",
+        "fault_desc": "quick, intense peak - straight before and after",
+        "severity": "X"
     }
 }
 
@@ -57,6 +113,9 @@ class KnowledgeGraphGenerator:
 
 if __name__ == '__main__':
     kg_gen = KnowledgeGraphGenerator()
-    kg_gen.extend_knowledge_graph_with_sensor_fault_data(
-        SYMBOLIC_FAULT_INFO[1]['name'], SYMBOLIC_FAULT_INFO[1]['fault_desc'], SYMBOLIC_FAULT_INFO[1]['severity']
-    )
+    for class_idx in range(1, 13):
+        kg_gen.extend_knowledge_graph_with_sensor_fault_data(
+            SYMBOLIC_FAULT_INFO[class_idx]['name'],
+            SYMBOLIC_FAULT_INFO[class_idx]['fault_desc'],
+            SYMBOLIC_FAULT_INFO[class_idx]['severity']
+        )
