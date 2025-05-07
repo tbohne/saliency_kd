455edf2f641fbdda90cabfc0ed7259ecd89eefa0
tbohne <tim.bohne@dfki.de>
2025-05-07 13:49:39 +0200

    kgqt - extend fault desc query

--- saliency_kd/knowledge_graph_query_tool.py
+++ saliency_kd/knowledge_graph_query_tool.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @author Tim Bohne
 
-from typing import List
+from typing import List, Tuple
 
 from saliency_kd.config import ONTOLOGY_PREFIX, FUSEKI_URL
 from saliency_kd.connection_controller import ConnectionController
@@ -32,7 +32,7 @@ class KnowledgeGraphQueryTool:
         """
         return "<" + self.ontology_prefix.replace('#', '#' + entry) + ">"
 
-    def query_all_fault_desc(self, verbose: bool = True) -> List[str]:
+    def query_all_fault_desc(self, verbose: bool = True) -> List[Tuple[str, str]]:
         """
         Queries all symbolic fault descriptions stored in the knowledge graph.
 
@@ -46,13 +46,16 @@ class KnowledgeGraphQueryTool:
 
         sensor_fault_entry = self.complete_ontology_entry('SensorFault')
         desc_entry = self.complete_ontology_entry('fault_desc')
+        name_entry = self.complete_ontology_entry('name')
         s = f"""
-            SELECT ?fault_desc WHERE {{
+            SELECT ?fault_name ?fault_desc WHERE {{
                 ?sensor_fault a {sensor_fault_entry} .
                 ?sensor_fault {desc_entry} ?fault_desc .
+                ?sensor_fault {name_entry} ?fault_name .
             }}
             """
-        return [row['fault_desc']['value'] for row in self.fuseki_connection.query_knowledge_graph(s, verbose)]
+        return [(row['fault_name']['value'], row['fault_desc']['value'])
+                for row in self.fuseki_connection.query_knowledge_graph(s, verbose)]
 
     @staticmethod
     def print_res(res: List[str]) -> None:
