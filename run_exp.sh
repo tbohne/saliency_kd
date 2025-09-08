a71aebc72e2c0a75832c1cdd2ec6c6d072493f24
tbohne <tim.bohne@dfki.de>
2025-09-08 16:48:51 +0200

    extend LLM analysis call in exp script

--- run_exp.sh
+++ run_exp.sh
@@ -1,3 +1,3 @@
 for i in {1..20}; do
-    python saliency_kd/llm_analysis.py >> output.txt
+    python saliency_kd/llm_analysis.py --mode ts --input llm_input/Mallat/class_0/centroids4llm.npy --model o3-2025-04-16 >> output.txt
 done
